d3e570a924650ac6fe8e6752115fc924b58ee973
Claire Chen <clairechen@claires-air.wifi.local.cmu.edu>
2021-11-06 20:57:10 -0400

    deleted frontend folder

--- flaskapi.py
+++ flaskapi.py
@@ -1,8 +1,8 @@
 # Source: partially from Toward Data Science
 from flask import Flask
 from flask_restful import Resource, Api, reqparse
-import pandas as pd
-import ast
+# import pandas as pd
+# import ast
 
 app = Flask(__name__)
 api = Api(app)
