09cedf816125a7aa0be040716af93394fef3b6f5
Claire Chen <clairechen@claires-air.wifi.local.cmu.edu>
2021-11-06 20:45:31 -0400

    added api files

--- flaskapi.py
+++ flaskapi.py
@@ -7,3 +7,29 @@ import ast
 app = Flask(__name__)
 api = Api(app)
 
+# File path for storing the slang words
+@app.route('/api',methods=['GET'])
+def apiHandler():
+    return {
+        "slang" : "definition"
+    }
+ 
+if __name__ == '__main__':
+    app.run(debug=True)
+# Input: a set of slang words {}
+# Dictionary
+# Keys : values
+# Slang : ('definition', ['example sentence', ])
+# def ApiHandler(Resources):
+#     def get(self):
+#         return {
+
+#         }
+
+#     def post(self):
+#         return 
+
+# genzlator.ml/
+# Localhost: http://127.0.0.1:5000
+# api.add_resource(Slang, '/slang')
+# api.add_resource(NormalWord, '/normalword')
